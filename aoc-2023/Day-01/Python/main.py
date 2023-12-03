0d2f2d28e83fa096f27f572ccb486176f4711d4d
RussellDash332 <russellsaerang@gmail.com>
2023-12-03 16:30:55 +0800

    Shorten code for Day 01

--- aoc-2023/Day-01/Python/main.py
+++ aoc-2023/Day-01/Python/main.py
@@ -1,16 +1,11 @@
 import sys
 t = u = 0
 m = dict(zip(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', *map(str, range(1, 10))], [*range(1, 10)]*2))
+n = [*m, *map(str, range(10))]
 for l in sys.stdin:
     s = [*map(int, filter(lambda x: x.isnumeric(), l))]
-    f = []
-    for i in [*m, *map(str, range(10))]:
-        f.append((i, l.find(i)))
-        if f[-1][1] == -1: f.pop()
-        f.append((i, l.rfind(i)))
-        if f[-1][1] == -1: f.pop()
-    f = sorted({*f}, key=lambda x: x[1])
     t += 10*s[0] + s[-1]
+    f = sorted(filter(lambda x: x[1]+1, {*((i, t(i)) for i in n for t in (l.find, l.rfind))}), key=lambda x: x[1])
     u += 10*m[f[0][0]] + m[f[-1][0]]
 print('Part 1:', t)
 print('Part 2:', u)
