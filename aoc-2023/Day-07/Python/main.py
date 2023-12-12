45d710c3787e990c966b969c5266deacba876332
RussellDash332 <russellsaerang@gmail.com>
2023-12-12 16:18:18 +0700

    Update Day 07

--- aoc-2023/Day-07/Python/main.py
+++ aoc-2023/Day-07/Python/main.py
@@ -1,1 +1,9 @@
-import sys;from collections import*;m=dict(zip('TJQKA',':;<=>'));r=lambda f:[max(f),-len(f)];d=[*map(str.split,sys.stdin)];s=lambda t:sum((i+1)*int(e[1])for i,e in enumerate(sorted(d,key=lambda c:max(r(Counter(c[0].replace(t,i)).values())for i in c[0])+[m.get(i,i)for i in c[0]])));print('Part 1:',s('@'));m['J']='1';print('Part 2:',s('J'))
+import sys; from collections import *
+m = dict(zip('TJQKA', ':;<=>'))
+r = lambda f: [max(f), -len(f)]
+d = [*map(str.split, sys.stdin)]
+def solve(t):
+    sd = sorted(d, key=lambda c: max(r(Counter(c[0].replace(t, i)).values()) for i in c[0])+[m.get(i, i) for i in c[0]])
+    return sum((i+1)*int(e[1]) for i,e in enumerate(sd))
+print('Part 1:', solve('@'))
+m['J'] = '1'; print('Part 2:', solve('J'))
