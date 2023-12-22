cd00857a748f0c7535f0a6dcd9067aa282608bb5
RussellDash332 <russellsaerang@gmail.com>
2023-12-22 21:06:29 +0700

    Optimize Day 12

--- aoc-2023/Day-12/Python/main.py
+++ aoc-2023/Day-12/Python/main.py
@@ -1,22 +1,22 @@
 import sys
 
 def solve(l, p):
-    m = {}
+    q = len(p); L = len(l); m = {}
     def f(i, j, c):
+        if i==L: return (c==0 and j==q)or(c==p[-1] and j==q-1)
         k = (i, j, c)
         if k in m: return m[k]
-        if i==len(l): return (c==p[-1] and j==len(p)-1)|(c==0 and j==len(p))
         r = 0
-        if l[i] in '?#': r += f(i+1, j, c+1)
-        if l[i] in '?.':
+        if l[i] > '#':
             if c == 0: r += f(i+1, j, 0)
-            elif c > 0 and j < len(p) and p[j] == c: r += f(i+1, j+1, 0)
+            elif j < q and p[j] == c: r += f(i+1, j+1, 0)
+        if l[i] != '.': r += f(i+1, j, c+1)
         m[k] = r; return r
-    return f(0, 0, 0)
+    return complex(f(0, 0, 0), m[(4*L//5+1, 4*q//5, 0)])
 
-a = b = 0
+a = 0
 for l in sys.stdin:
-    l, p = l.split(); p = [*map(int, p.split(','))]
-    a += solve(l, p); b += solve('?'.join(5*[l]), 5*p)
-print('Part 1:', a)
-print('Part 2:', b)
+    l, p = l.split()
+    a += solve('?'.join(5*[l]), 5*[*map(int, p.split(','))])
+print('Part 1:', int(a.imag))
+print('Part 2:', int(a.real))
