6aeb0bc2493e2c5722bbc3301ffa44d0123da61c
RussellDash332 <russellsaerang@gmail.com>
2023-12-22 17:55:35 +0700

    Optimize Day 22

--- aoc-2023/Day-22/Python/main.py
+++ aoc-2023/Day-22/Python/main.py
@@ -1,32 +1,37 @@
 import sys, re, itertools
-B = {}; S = sorted([[*map(int, re.findall('[-\d]+', l))] for l in sys.stdin], key=lambda x: min(x[2],x[5]))
-G = [set() for _ in S]; M = len(S); I = [0]*M
+
+# [-\d+] -> \d+ because there doesn't seem to be any negative number
+# min(x[2],x[5]) -> x[2] because L < R
+# range(min(a,b), max(a,b)+1) -> range(a,b+1) because of the above assumption
+
+B = {}; S = sorted(([*map(int, re.findall('\d+',l))] for l in sys.stdin), key=lambda x: x[2])
+M = len(S); I = [0]*M
 
 # list cubes
 def lc(x1, y1, z1, x2, y2, z2):
-    return itertools.product(range(min(x1,x2), max(x1,x2)+1), range(min(y1,y2), max(y1,y2)+1), range(min(z1,z2), max(z1,z2)+1))
+    return itertools.product(range(x1, x2+1), range(y1, y2+1), range(z1, z2+1))
 
 # count falls
 def cf(i):
-    J = I.copy(); s = [i]; d = {i}
-    while s:
-        d.add(u:=s.pop())
+    J = [*I]; s = [i]; d = -1
+    for u in s:
+        d += 1
         for v in G[u]:
             J[v] -= 1
             if J[v] == 0: s.append(v)
-    return len(d)-1
+    return d
 
 # simulate dropping
 for i, j in enumerate(S):
-    while j[2]*j[5]:
+    while j[2]:
         j[2] -= 1; j[5] -= 1
-        if any(B.get(t,i)-i for t in lc(*j)): j[2] += 1; j[5] += 1; break
+        if any(t in B and B[t] != i for t in lc(*j)): j[2] += 1; j[5] += 1; break
     for t in lc(*j): B[t] = i
 
 # connect supports
+G = []
 for i in range(M):
-    for x,y,z in lc(*S[i]):
-        if B.get(j:=(x,y,z+1),i)-i: G[i].add(B[j])
+    G.append({B[j] for *xy,z in lc(*S[i]) if (j:=(*xy,z+1)) in B and B[j] != i})
     for j in G[i]: I[j] += 1
 
 print('Part 1:', sum(all(I[j]-1 for j in v) for v in G))
