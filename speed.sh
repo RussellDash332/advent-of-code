543712afa23ad8fe6804d7b9e6e320f3fc7f1622
RussellDash332 <russellsaerang@gmail.com>
2023-12-28 21:21:34 +0700

    Wrap up AOC 2023

--- speed.sh
+++ speed.sh
@@ -2,5 +2,8 @@ export Y=2023
 export TIMEFORMAT='Time: %3R seconds'
 cd ~/advent-of-code/aoc-$Y
 for i in $(seq -f '%02g' 1 25)
-do echo "Day $i:" && aocd $Y $i > Day-$i/$i.in && time python Day-$i/Python/main.py < Day-$i/$i.in && echo
+do aocd $Y $i > Day-$i/$i.in
+done
+for i in $(seq -f '%02g' 1 25)
+do echo "Day $i:" && time python Day-$i/Python/main.py < Day-$i/$i.in && echo
 done
