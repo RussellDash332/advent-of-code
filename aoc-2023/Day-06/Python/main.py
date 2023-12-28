efcb3d0b419043836a297ee2c3bc36b70a0451a3
RussellDash332 <russellsaerang@gmail.com>
2023-12-28 17:31:56 +0700

    Tidy up Day 06

--- aoc-2023/Day-06/Python/main.py
+++ aoc-2023/Day-06/Python/main.py
@@ -1,3 +1,5 @@
-f=1;l=[input().split()[1:]for _ in'..'];g=lambda a,b:a-1-2*int((a-(a*a-4*b)**0.5)/2)
-for i in zip(*l):f*=g(*map(int,i))
-print('Part 1:',f,'\nPart 2:',g(*map(int,map(''.join,l))))
+import math
+l = [input().split()[1:] for _ in '..']
+g = lambda a,b: a-1-2*int((a-(a*a-4*b)**0.5)/2)
+print('Part 1:', math.prod(g(*map(int,i)) for i in zip(*l)))
+print('Part 2:', g(*map(int,map(''.join, l))))
