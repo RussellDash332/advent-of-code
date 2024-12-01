3677d8c1f0db49962b9afde9f300df9555497514
RussellDash332 <russellsaerang@gmail.com>
2024-12-02 01:00:46 +0800

    Add Day 01

--- populate.sh
+++ populate.sh
@@ -1,4 +1,4 @@
-export Y=2023
+export Y=2024
 cd ~/advent-of-code/aoc-$Y
 for i in $(seq -f '%02g' 1 25)
 do mkdir -p Day-$i && touch Day-$i/$i.in && mkdir -p Day-$i/Python && touch Day-$i/Python/main.py
