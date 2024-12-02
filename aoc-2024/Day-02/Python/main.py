5d56af0e2dcaffb2d3ef786fcaf99007e6c40a4b
RussellDash332 <russellsaerang@gmail.com>
2024-12-03 00:27:28 +0800

    Add Day 02

---
+++ aoc-2024/Day-02/Python/main.py
@@ -0,0 +1,1 @@
+a=[[*map(int,r.split())]for r in open(0)];f=lambda t:sum(any((z:=[*zip(s:=r[:i]+r[i+1:],s[1:])])*(all(0<x-y<4for x,y in z)+all(0<y-x<4for x,y in z))for i in range(len(r)*t,len(r)+1))for r in a);print('Part 1:',f(1),'\nPart 2:',f(0))
