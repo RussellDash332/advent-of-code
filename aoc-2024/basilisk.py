c79c89d91f038cd0705e349dfc1580766122ee80
RussellDash332 <russellsaerang@gmail.com>
2024-12-03 19:16:24 +0800

    Modify basilisk

--- aoc-2024/basilisk.py
+++ aoc-2024/basilisk.py
@@ -2,4 +2,6 @@
 a,b=map(sorted,zip(*(map(int,x.split())for x in open(0))));print('Part 1:',sum(abs(x-y)for x,y in zip(a,b)),'\nPart 2:',sum(x*b.count(x)for x in a))
 # Day 02: 232
 a=[[*map(int,r.split())]for r in open(0)];f=lambda t:sum(any((z:=[*zip(s:=r[:i]+r[i+1:],s[1:])])*(all(0<x-y<4for x,y in z)+all(0<y-x<4for x,y in z))for i in range(len(r)*t,len(r)+1))for r in a);print('Part 1:',f(1),'\nPart 2:',f(0))
-# Day 03: 
+# Day 03: 196
+import re;t=('do()'+open(0).read()).split("don't()");f=lambda p:sum(int(a)*int(b)for r in t for a,b in re.findall('mul\((\d+),(\d+)\)',r[r.find('do()')*p:]));print('Part 1:',f(0),'\nPart 2:',f(1))
+# Day 04: 
--- aoc-2024/run_basilisk.py
+++ aoc-2024/run_basilisk.py
@@ -3,5 +3,5 @@ with open('basilisk.py') as f: lines = f.readlines()
 for day in range(1, 26):
     print(f'Day {day:02d}')
     t = time.time()
-    os.system(f'''python3.8 -c "{lines[2*day-1]}" < Day-{day:02d}/{day:02d}.in''')
+    os.system(f'''python3.8 -c "{lines[2*day-1].replace(chr(34),3*chr(39))}" < Day-{day:02d}/{day:02d}.in''')
     print(f'Time: {round(time.time()-t, 3)} seconds\n')
