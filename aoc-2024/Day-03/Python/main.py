f147d3216bab0a7466fa72cd36ce3789ab970ccd
RussellDash332 <russellsaerang@gmail.com>
2024-12-03 14:08:23 +0800

    Add Day 03

---
+++ aoc-2024/Day-03/Python/main.py
@@ -0,0 +1,1 @@
+import re;t=('do()'+open(0).read()).split("don't()");f=lambda p:sum(int(a)*int(b)for r in t for a,b in re.findall('mul\((\d+),(\d+)\)',r[r.find('do()')*p:]));print('Part 1:',f(0),'\nPart 2:',f(1))
