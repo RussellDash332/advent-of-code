e74b9f345a790cde04ec2036c4e508a692a1b4d8
RussellDash332 <russellsaerang@gmail.com>
2024-12-06 14:50:25 +0800

    Update basilisk

--- aoc-2024/basilisk.py
+++ aoc-2024/basilisk.py
@@ -8,4 +8,6 @@ import re;t=('do()'+open(0).read()).split("don't()");f=lambda p:sum(int(a)*int(b
 z=range;k=z(-1,2);r=len(m:=[*open(0)]);c=len(m[-1]);print('Part 1:',sum(''.join(m[i//c+t*p][i%c+t*q]for t in z(4))=='XMAS'for i in z(r*c)for p in k for q in k if r>i//c+3*p>-1<i%c+3*q<c),'\nPart 2:',sum(m[i+1][j+1]<'B'!={m[i][j],m[i+2][j+2]}&{m[i+2][j],m[i][j+2]}=={*'MS'}for i in z(r-2)for j in z(c-2)))
 # Day 05: 358
 r=range;m=z=s=0;p={};u=p.get;d={0};[len(l)==1and[m:=1]or m and[k:=len(a:=[*map(int,l.split(','))])//2,e:=a[k],c:=1,[a[i]in u(a[j],d)or[c:=0,t:=a[i],f:=a.__setitem__,f(i,a[j]),f(j,t)]for i in r(len(a))for j in r(i)],z:=z+e*c,s:=s+(1-c)*a[k]]or[a:=[*map(int,l.split('|'))],p.__setitem__(a[0],u(a[0],d)|{a[1]})]for l in open(0)];print('Part 1:',z,'\nPart 2:',s)
-# Day 06: 
+# Day 06: 452
+S=list.__setitem__;r=len(m:=[*map(list,open(X:=0))]);c=len(m[-1]);R=range;[[z:=0,[(T:=m[a])[b]=='^.'[p]and[f:=[*(X:=X or(a,b))],i:=f[0],j:=f[1],S(T,b,'^#'[p]),d:={0},x:=-1,y:=0,l:=0,[(r>i>-1<j<c)*(2-p-l)and[w:=(i,j),[[u:=y,y:=-x,x:=u]for _ in'.'*4if r>i+x>-1<j+y<c!=m[i+x][j+y]<'.'],i:=i+x,j:=j+y,t:=w+(i,j)*p,l:=l|(t in d),d.add(t)]for _ in'.'*r*c],S(T,b,'^.'[p]),z:=z+[len(d)-1,l][p]]for a in R(r)for b in R(c)],print(f'Part {p+1}:',z)]for p in(0,1)]
+# Day 07: 
