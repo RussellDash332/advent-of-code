310dc63b286255a6e19f7c745523f873bb150747
RussellDash332 <russellsaerang@gmail.com>
2024-12-09 01:49:45 +0800

    Include GCD for Day 08 just in case

--- aoc-2024/Day-08/Python/main.py
+++ aoc-2024/Day-08/Python/main.py
@@ -1,5 +1,5 @@
 # Partly golfed
-R=len(m:=[*open(0)]);C=len(m[-1]);N={};A=set();B=set()
+import math;R=len(m:=[*open(0)]);C=len(m[-1]);N={};A=set();B=set()
 for i in range(R):
     for j in range(C):N[m[i][j]]=N.get(m[i][j],[])+[(i,j)]
 for i in N:
@@ -8,6 +8,7 @@ for i in N:
    for c,d in N[i]:
     x=a-c;y=b-d;e=c;f=d
     if x or y:
+     g=math.gcd(x,y);x//=g;y//=g
      if R>c+2*x>-1<d+2*y<C:A.add((c+2*x,d+2*y))
      while R>e+x>-1<f+y<C:e+=x;f+=y;B.add((e,f))
 print('Part 1:',len(A),'\nPart 2:',len(B))
