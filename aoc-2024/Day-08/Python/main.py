858be1be03d5bd8efee2f89dafc4b0d87800223d
RussellDash332 <russellsaerang@gmail.com>
2024-12-08 17:18:23 +0800

    Add Day 08

---
+++ aoc-2024/Day-08/Python/main.py
@@ -0,0 +1,13 @@
+# Partly golfed
+R=len(m:=[*open(0)]);C=len(m[-1]);N={};A=set();B=set()
+for i in range(R):
+    for j in range(C):N[m[i][j]]=N.get(m[i][j],[])+[(i,j)]
+for i in N:
+ if i>'.':
+  for a,b in N[i]:
+   for c,d in N[i]:
+    x=a-c;y=b-d;e=c;f=d
+    if x or y:
+     if R>c+2*x>-1<d+2*y<C:A.add((c+2*x,d+2*y))
+     while R>e+x>-1<f+y<C:e+=x;f+=y;B.add((e,f))
+print('Part 1:',len(A),'\nPart 2:',len(B))
