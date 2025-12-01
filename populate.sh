f42da82e50090818f7bdd82ed08b2485dae4fb6c
Russell Saerang <63991775+RussellDash332@users.noreply.github.com>
2025-12-01 06:09:47 +0000

    Modify populate script

--- populate.sh
+++ populate.sh
@@ -1,5 +1,5 @@
-export Y=2024
-cd ~/advent-of-code/aoc-$Y
-for i in $(seq -f '%02g' 1 25)
+export Y=2025
+cd /workspaces/advent-of-code/aoc-$Y
+for i in $(seq -f '%02g' 1 12)
 do mkdir -p Day-$i && touch Day-$i/$i.in && mkdir -p Day-$i/Python && touch Day-$i/Python/main.py
 done
