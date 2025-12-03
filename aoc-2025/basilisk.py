eeb17f386536be31fa3925b9ecbd6bb33d96d287
Russell Saerang <63991775+RussellDash332@users.noreply.github.com>
2025-12-03 06:09:25 +0000

    Modify basilisk

--- aoc-2025/basilisk.py
+++ aoc-2025/basilisk.py
@@ -1,4 +1,6 @@
 # Day 01: 145
 p=50;q=[[p:=(p+2*(x[0]>'Q')-1)%100for _ in'.'*int(x[1:])]for x in open(0)];print('Part 1:',sum(r[-1]<1for r in q),'\nPart 2:',sum(q,[]).count(0))
-# Day 02: 227
-v=[*map(int,input().replace('-',',').split(','))];f=lambda t:sum([k:=len(s:=str(i)),i*any(s==l*s[:k//l]for l in range(2,[2,k][t]+1))][1]for a,b in zip(v[::2],v[1::2])for i in range(a,b+1));print('Part 1:',f(0),'\nPart 2:',f(1))
+# Day 02: 196
+import re;v=[[*map(int,l.split('-'))]for l in input().split(',')];f=lambda t:sum(i for a,b in v for i in range(a,b+1)if re.match(rf'^(\d+)\1{t}$',str(i)));print('Part 1:',f(''),'\nPart 2:',f('+'))
+# Day 03: 187
+from functools import*;f=cache(lambda x,n:max(f(y:=x//10,n),10*f(y,n-1)+x%10)if x*n else 0);v=[*map(int,open(0))];print('Part 1:',sum(f(x,2)for x in v),'\nPart 2:',sum(f(x,12)for x in v))
