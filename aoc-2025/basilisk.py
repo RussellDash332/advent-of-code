970094dc656641cea01371b4aef49b49223cd175
Russell Saerang <63991775+RussellDash332@users.noreply.github.com>
2025-12-04 21:33:59 +0800

    Modify basilisk

--- aoc-2025/basilisk.py
+++ aoc-2025/basilisk.py
@@ -1,5 +1,5 @@
-# Day 01: 145
-p=50;q=[[p:=(p+2*(x[0]>'Q')-1)%100for _ in'.'*int(x[1:])]for x in open(0)];print('Part 1:',sum(r[-1]<1for r in q),'\nPart 2:',sum(q,[]).count(0))
+# Day 01: 142
+p=50;q=[[p:=(p+2*(x>'Q')-1)%100for _ in'.'*int(x[1:])]for x in open(0)];print('Part 1:',sum(r[-1]<1for r in q),'\nPart 2:',sum(q,[]).count(0))
 # Day 02: 196
 import re;v=[[*map(int,l.split('-'))]for l in input().split(',')];f=lambda t:sum(i for a,b in v for i in range(a,b+1)if re.match(rf'^(\d+)\1{t}$',str(i)));print('Part 1:',f(''),'\nPart 2:',f('+'))
 # Day 03: 187
