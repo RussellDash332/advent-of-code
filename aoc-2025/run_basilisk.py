2f9e31dd96c4a71371dfd860c43ba1fa21db587c
RussellDash332 <russellsaerang@gmail.com>
2025-12-12 16:08:12 +0800

    Ignore syntax warnings

--- aoc-2025/run_basilisk.py
+++ aoc-2025/run_basilisk.py
@@ -4,5 +4,5 @@ with open('basilisk.py') as f: lines = f.readlines()
 for day in range(d, 13):
     print(f'Day {day:02d}')
     t = time.time()
-    os.system(f'''python -c "{lines[2*day-1].replace(chr(34),3*chr(39))}" < Day-{day:02d}/{day:02d}.in''')
+    os.system(f'''python -W ignore -c "{lines[2*day-1].replace(chr(34),3*chr(39))}" < Day-{day:02d}/{day:02d}.in''')
     print(f'Time: {round(time.time()-t, 3)} seconds\n')
